2176f2e83f941d9ce9ee12ee3c0520a8ef29007a
Y N <23240959@turan-edu.kz>
2023-09-07 15:45:04 +0600

    first

---
+++ main.py
@@ -0,0 +1,6 @@
+while True:
+    print("Пожалуйста введите 2 числа")
+    first = int(input())
+    second = int(input())
+    print("Выберите операцию: 1 - сложение; 2 - вычитание; 3 - умножение; 4 - деление")
+    operation = int(input())

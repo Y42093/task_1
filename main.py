a1d86577dfebbbad8a7807dc01ed0ace08c7f3b5
Y N <23240959@turan-edu.kz>
2023-09-07 15:45:47 +0600

    second

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ while True:
     second = int(input())
     print("Выберите операцию: 1 - сложение; 2 - вычитание; 3 - умножение; 4 - деление")
     operation = int(input())
+    if operation == 1:
+        print(first + second)
+    elif operation == 2:
+        print(first - second)
+    elif operation == 3:
+        print(first * second)
+    elif operation == 4 and second != 0:
+        print(first / second)
+    else:
+        print("Некорректный ввод")
